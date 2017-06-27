c63d3810f816c5535d06636261da32c1278975c5
TheRoyalRaider <bjstoeber@icloud.com>
2017-06-27 19:32:06 -0400

    Added Ore Blocks

--- OreGenPlus.java
+++ OreGenPlus.java
@@ -10,7 +10,21 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class OreGenPlus{
 	
+	//Coal
+	public static Block blockCoalOre //Coal
 	
+	//Iron
+	public static Block blockSiderite //Poor Iron Ore
+	public static Block blockGoethite //Normal Iron Ore
+	public static Block blockMagnetite //Rich Iron Ore
+	
+	//Copper
+	public static Block blockChalcopyrite //Poor Copper Ore
+	public static Block blockAzurite //Normal Copper Ore
+	public static Block blockMalachite //Rich Copper Ore
+	
+	//Aluminum
+	public static Block block
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
