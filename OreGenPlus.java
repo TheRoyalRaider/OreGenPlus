0639d7dbdd2f6164b41687158378fbb747747bda
TheRoyalRaider <bjstoeber@icloud.com>
2017-06-27 18:40:41 -0400

    Added OreGenPlus.java

---
+++ OreGenPlus.java
@@ -0,0 +1,29 @@
+package northerneclipse.oregenplus;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "ogp", name = "OreGenPlus", version = "1.0")
+
+public class OreGenPlus{
+	
+	
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void Init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+}
